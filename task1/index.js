d35fddd501a873f25ef5b224ab7651d16f673213
Gravit09 <garvitchoudhary09@gmail.com>
2025-04-18 11:38:04 +0530

    avg and prime added

--- task1/index.js
+++ task1/index.js
@@ -1,12 +1,34 @@
 import express from "express";
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-let numbers = [];
+const PORT = 3000;
+const WINDOW_SIZE = 10;
 
-app.post("/", () => {});
+let numberStore = [];
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const calculateAverage = (numbers) => {
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return Number((sum / numbers.length).toFixed(2));
+};
+
+const generatePrimes = (count = 10) => {
+  const primes = [];
+  let num = 2;
+  while (primes.length < count) {
+    let isPrime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) primes.push(num);
+    num++;
+  }
+  return primes;
+};
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
